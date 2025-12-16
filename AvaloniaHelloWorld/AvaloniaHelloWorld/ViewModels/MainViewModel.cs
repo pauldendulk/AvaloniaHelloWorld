91b891d7f16455606a1903b60f43cb913c3488fe
pauldendulk <pauldendulk@gmail.com>
2025-12-16 09:24:13 +0100

    Add hello world app with deploy

---
+++ AvaloniaHelloWorld/AvaloniaHelloWorld/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaHelloWorld.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _greeting = "Hello, world!";
+}
---
+++ AvaloniaHelloWorld/AvaloniaHelloWorld/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaHelloWorld.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
